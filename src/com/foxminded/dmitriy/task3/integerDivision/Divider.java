ccadd209cc5ef50821962f1038a493461d3e8723
beliy@kaz.saturn.net <beliy@kaz.saturn.net>
2018-08-16 15:32:58 +0300

    fixed all tests, added oportunity for negative dividing

--- src/com/foxminded/dmitriy/task3/integerDivision/Divider.java
+++ src/com/foxminded/dmitriy/task3/integerDivision/Divider.java
@@ -9,15 +9,15 @@ public class Divider {
     public int showDivision(int dividend, int divisor) {
         if (divisor == 0)
             throw new IllegalArgumentException("Argument 'divisor' is 0!");
-        boolean positive = isPositive(dividend, divisor);
 
         int quotient = dividend / divisor;
         int remainder = dividend % divisor;
 
         int absDividend = Math.abs(dividend);
         int absDivisor = Math.abs(divisor);
+        int absQuotient = Math.abs(quotient);
 
-        String sQuotient = String.valueOf(quotient);
+        String sQuotient = String.valueOf(absQuotient);
         String sRemainder = String.valueOf(remainder);
         String sDividend = UNDERSCORE + String.valueOf(dividend);
         String sDivisor = String.valueOf(divisor);
@@ -37,64 +37,65 @@ public class Divider {
         }
 
         log.append(UNDERSCORE + dividend + PIPE + divisor + "\n");
-        boolean isFirst = true;
+        boolean isBlockInfo = true;
         int multiplicand = 1;
-        int tempDiv = dividend;
+        int tempDiv = absDividend;
         int index;
 
-        for (index = 1; index < sQuotient.length(); index++){
-            multiplicand *=10;
+        for (index = 1; index < sQuotient.length(); index++) {
+            multiplicand *= 10;
         }
-            for (index = 1; index <= sQuotient.length(); index++, isFirst = false, multiplicand /= 10) {
-                int rightSpaces = sQuotient.length() - index; // ?
-                int digit = Integer.parseInt(sQuotient.substring(index - 1, index));
-                int subtrahend = digit * divisor;
-                if (digit != 0) {
+        for (index = 1; index <= sQuotient.length(); index++, isBlockInfo = false, multiplicand /= 10) {
+            int rightSpaces = sQuotient.length() - index; // ?
+            int digit = Integer.parseInt(sQuotient.substring(index - 1, index));
+            int subtrahend = digit * absDivisor;
+            if (digit != 0) {
 
-                    // уменьшаемое
-                    String sSubtrahend = String.valueOf(subtrahend);
-                    String sMinuend;
+                String sSubtrahend = String.valueOf(subtrahend);
+                String sMinuend;
 
-                    if (isFirst) {
-                        sMinuend = "";
-                    } else {
-                        sMinuend = String.valueOf(tempDiv);
-                        sMinuend = sMinuend.substring(0, sMinuend.length() - rightSpaces);
-                    }
-                    int minuendLeftSpaces = isFirst ? 0 : dividendLength - rightSpaces - sMinuend.length();
-
-                    if (!isFirst) {
-                        log.append(SPACE);
-                        append(log, SPACE, minuendLeftSpaces);
-                        log.append(sMinuend);
-                        append(log, SPACE, rightSpaces);
-                        log.append("\n");
-                    }
-
-                    // вычитаемое
-                    int subtrahendLeftSpaces = dividendLength - rightSpaces - sSubtrahend.length();
-                    append(log, SPACE, subtrahendLeftSpaces);
-                    log.append(MINUS).append(subtrahend);
-                    append(log, SPACE, rightSpaces);
-                    if (isFirst) {
-                        log.append(PIPE + "--");
-                        append(log, MINUS, dividendLength);
-                    }
-                    log.append("\n");
-
+                if (isBlockInfo) {
+                    sMinuend = "";
+                } else {
+                    sMinuend = String.valueOf(tempDiv);
+                    sMinuend = sMinuend.substring(0, sMinuend.length() - rightSpaces);
+                }
+                int minuendLeftSpaces = isBlockInfo ? 0 : dividendLength - rightSpaces - sMinuend.length();
+                if (!isBlockInfo) {
                     log.append(SPACE);
                     append(log, SPACE, minuendLeftSpaces);
-                    int limit = isFirst ? dividendLength - rightSpaces : sMinuend.length();
-                    append(log, MINUS, limit);
+                    log.append(sMinuend);
                     append(log, SPACE, rightSpaces);
-
-                    if (isFirst) {
-                        log.append(PIPE).append(sQuotient);
-                    }
-                    log.append('\n');
+                    log.append("\n");
                 }
-                tempDiv -= subtrahend * multiplicand;
+
+                int subtrahendLeftSpaces = dividendLength - rightSpaces - sSubtrahend.length();
+                append(log, SPACE, subtrahendLeftSpaces);
+                log.append(MINUS).append(subtrahend);
+                append(log, SPACE, rightSpaces);
+                if (isBlockInfo) {
+                    log.append(PIPE + "--");
+                    append(log, MINUS, dividendLength);
+                }
+                log.append("\n");
+
+                log.append(SPACE);
+                append(log, SPACE, minuendLeftSpaces);
+
+                int tempDivLength = String.valueOf(Math.abs(tempDiv)).length();
+                int limit = isBlockInfo ? tempDivLength - rightSpaces : sMinuend.length();
+                append(log, MINUS, limit);
+                append(log, SPACE, rightSpaces);
+
+                if (isBlockInfo) {
+                    if (dividend < 0)
+                        log.append(SPACE);
+                    log.append(PIPE).append(quotient);
+                }
+                log.append('\n');
             }
+            tempDiv -= subtrahend * multiplicand;
+        }
 
         log.append(SPACE);
         index = dividendLength - sRemainder.length();
@@ -102,15 +103,11 @@ public class Divider {
         log.append(sRemainder);
         System.out.println(log);
 
-        return positive ? quotient : -quotient;
+        return quotient;
     }
 
     private void append(StringBuilder log, String delimiter, int length) {
         for (int i = 0; i < length; i++)
             log.append(delimiter);
-    }
-
-    private boolean isPositive(int dividend, int divisor) {
-        return ((dividend >= 0 && divisor > 0) || (dividend < 0 && divisor < 0));
     }
 }
