161cd974309ff05778a2d5e35501bff0e0927ace
beliy@kaz.saturn.net <beliy@kaz.saturn.net>
2018-08-17 15:24:40 +0300

    division only

--- src/com/foxminded/dmitriy/task3/integerDivision/Divider.java
+++ src/com/foxminded/dmitriy/task3/integerDivision/Divider.java
@@ -5,29 +5,32 @@ public class Divider {
     private static final String PIPE = "|";
     private static final String MINUS = "-";
     private static final String UNDERSCORE = "_";
+    private static final int MAX_DECIMAL = 9;
+    private static final int MULTIPLIER = 10;
 
     public String showDivision(int dividend, int divisor) {
         if (divisor == 0)
             throw new IllegalArgumentException("Argument divisor = '0'!");
 
         StringBuilder result = new StringBuilder();
-        int multiplier = 1;
+        int rank = 1;   // "1" нужно выводить в static final int startPosition? или это усложнит?
         int remainder;
 
-        while ((dividend / divisor) > 9) {
-            divisor *= 10;
-            multiplier *= 10;
+        while ((dividend / divisor) > MAX_DECIMAL) {
+            divisor *= MULTIPLIER;
+            rank *= MULTIPLIER;
         }
         result.append(dividend / divisor);
         remainder = dividend % divisor;
 
-        while (multiplier != 1) {
-            divisor /= 10;
-            multiplier /= 10;
+        while (rank != 1) {
+            divisor /= MULTIPLIER;
+            rank /= MULTIPLIER;
             dividend = remainder;
             result.append(dividend / divisor);
             remainder = dividend % divisor;
         }
+        result.append(" и в остатке " + remainder);
         return result.toString();
     }
 }
--- src/com/foxminded/dmitriy/task3/integerDivision/Main.java
+++ src/com/foxminded/dmitriy/task3/integerDivision/Main.java
@@ -3,8 +3,8 @@ package com.foxminded.dmitriy.task3.integerDivision;
 public class Main {
     public static void main(String[] args) {
         Divider divider = new Divider();
-        int dividend = 1234;
-        int divisor = 2;
+        int dividend = 1178945;
+        int divisor = 4;
 
         String result = divider.showDivision(dividend,divisor);
         System.out.println(result);
