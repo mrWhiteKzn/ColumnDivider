1fb4e6e9a8b70416dc46a79ec8ad12c875a4032f
beliy@kaz.saturn.net <beliy@kaz.saturn.net>
2018-08-17 14:29:53 +0300

    division only

--- src/com/foxminded/dmitriy/task3/integerDivision/Divider.java
+++ src/com/foxminded/dmitriy/task3/integerDivision/Divider.java
@@ -10,15 +10,11 @@ public class Divider {
         if (divisor == 0)
             throw new IllegalArgumentException("Argument divisor = '0'!");
 
-        StringBuilder result = new StringBuilder().append(UNDERSCORE + dividend + PIPE + divisor + "\n");
-        appendSecondLine(result, dividend, divisor);
         StringBuilder quotient = new StringBuilder();
-
-        boolean positive = isPositive(dividend, divisor);
         dividend = Math.abs(dividend);
         divisor = Math.abs(divisor);
         int multiplier = 1;
-        int remainder = 0;
+        int remainder;
 
         while ((dividend / divisor) > 9) {
             divisor *= 10;
@@ -35,30 +31,8 @@ public class Divider {
             remainder = dividend % divisor;
         }
         int divResult = Integer.valueOf(quotient.toString());
-        System.out.println(result);
-        return positive ? divResult : -divResult;
+        return divResult;
     }
 
-    private void appendSecondLine(StringBuilder result, int dividend, int divisor) {
-        int divResult = dividend / divisor;
-        String sDividend = String.valueOf(dividend);
-        int rightSpaces = String.valueOf(divResult).length() - 1;
-        int leftSpaces = sDividend.length() - rightSpaces - String.valueOf(divisor).length();
 
-        result.append(SPACE);
-        append(result, SPACE, leftSpaces);
-        result.append(divisor);
-        append(result, SPACE, rightSpaces);
-        result.append(PIPE);
-        for (int i = 0; i < String.valueOf(divResult).length(); i++)
-            result.append(MINUS);
-    }
-
-    private void append(StringBuilder log, String symbol, int length) {
-        for (int i = 0; i < length; i++) log.append(symbol);
-    }
-
-    private boolean isPositive(int dividend, int divisor) {
-        return ((dividend > 0 && divisor > 0) || (dividend < 0 && divisor < 0));
-    }
 }
--- src/com/foxminded/dmitriy/task3/integerDivision/Main.java
+++ src/com/foxminded/dmitriy/task3/integerDivision/Main.java
@@ -3,11 +3,10 @@ package com.foxminded.dmitriy.task3.integerDivision;
 public class Main {
     public static void main(String[] args) {
         Divider divider = new Divider();
-        int dividend = -1234 ;
-        int divisor = 75;
+        int dividend = 1234;
+        int divisor = 2;
 
         int result = divider.showDivision(dividend,divisor);
-        System.out.println("_______________________________");
         System.out.println(result);
     }
 }
