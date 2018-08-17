abc0dbef21170cea1be76956b8e4ab942c6b9c94
beliy@kaz.saturn.net <beliy@kaz.saturn.net>
2018-08-17 16:57:33 +0300

    added out, just thoughts. needs refactor

--- src/com/foxminded/dmitriy/task3/integerDivision/Divider.java
+++ src/com/foxminded/dmitriy/task3/integerDivision/Divider.java
@@ -16,22 +16,87 @@ public class Divider {
         StringBuilder result = new StringBuilder();
         int rank = START_POSITION;
         int remainder;
+        StringBuilder sb = new StringBuilder();
+        
+        // первая строка
+        int divResult = dividend / divisor;
+        System.out.println(UNDERSCORE + dividend + PIPE + divisor);
+        //
+
+        // service
+        int didivend2 = dividend;
 
         while ((dividend / divisor) > MAX_DECIMAL) {
             divisor *= MULTIPLIER;
             rank *= MULTIPLIER;
         }
-        result.append(dividend / divisor);
+//        result.append(dividend / divisor);
         remainder = dividend % divisor;
 
+        // вывод
+
+        int dividendLen = String.valueOf(UNDERSCORE + dividend).length();
+        int index = 1;
+        int rightSpaces = String.valueOf(divResult).length() - index;
+        int digit = Integer.valueOf(String.valueOf(divResult).charAt(0) - '0');
+        int subtrahend = divisor / rank * digit;
+        int leftSpaces = dividendLen - rightSpaces - String.valueOf(subtrahend).length();
+
+        append(sb, SPACE, leftSpaces);
+        sb.append(subtrahend);
+        append(sb, SPACE, rightSpaces);
+        sb.append(PIPE);
+
+        for (int i = 0; i < String.valueOf(divResult).length(); i++) {
+            sb.append(MINUS);
+        }
+        sb.append("\n");
+        //
+        // service
+//        didivend2 -= subtrahend * rank;
+        boolean first = true;
+
         while (rank != START_POSITION) {
+            didivend2 -= subtrahend * rank;
+            digit = Integer.valueOf(String.valueOf(divResult).charAt(index) - '0');
+            index++;
+            rightSpaces = String.valueOf(divResult).length() - index;
+
             divisor /= MULTIPLIER;
             rank /= MULTIPLIER;
             dividend = remainder;
-            result.append(dividend / divisor);
+//            result.append(dividend / divisor);
             remainder = dividend % divisor;
+
+            // вывод
+            String sMinuend = String.valueOf(didivend2);
+            sMinuend = sMinuend.substring(0, sMinuend.length() - rightSpaces);
+            leftSpaces = dividendLen - rightSpaces - sMinuend.length()-1;
+            append(sb, SPACE, leftSpaces);
+            sb.append(UNDERSCORE).append(sMinuend);
+            append(sb,SPACE, rightSpaces);
+            if(first){
+                sb.append(PIPE).append(divResult);
+                first = false;
+            }
+            sb.append("\n");
+
+            subtrahend = divisor / rank * digit;
+            int subtrahendLeftSpaces = dividendLen - rightSpaces - String.valueOf(subtrahend).length();
+            append(sb, SPACE, subtrahendLeftSpaces);
+            sb.append(subtrahend);
+            sb.append("\n");
+            //
         }
-        result.append(" и в остатке " + remainder);
+
+        index = dividendLen - String.valueOf(remainder).length();
+        append(sb, SPACE, index);
+        sb.append(remainder);
+        System.out.println(sb);
         return result.toString();
+    }
+
+    private void append(StringBuilder sb, String symbol, int count) {
+        for (int i = 0; i < count; i++) sb.append(symbol);
     }
 }
